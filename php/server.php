0d18005c83635ee1f1486bad8aacab697bca9184
ishkovruslan <rus.ishkov.04@gmail.com>
2025-03-12 09:15:26 +0200

    Updated encryption functions

--- php/server.php
+++ php/server.php
@@ -1,37 +1,38 @@
 <?php
 require_once 'php/access.php';
 
-// Функція для шифрування повідомлення
+/**
+ * Функція для шифрування повідомлення за допомогою AES-256-CBC.
+ */
 function encrypt($message, $key)
 {
-    $keyStr = pack('J', $key);
-    $keyLen = strlen($keyStr);
-    $encrypted = '';
-
-    for ($i = 0; $i < strlen($message); $i++) {
-        $messageChar = ord($message[$i]);
-        $keyChar = ord($keyStr[$i % $keyLen]);
-        $encrypted .= chr(($messageChar + $keyChar) % 256);
-    }
-
-    return base64_encode($encrypted);
+    $cipher = 'aes-256-cbc';
+    // Генеруємо 32-байтовий ключ із використанням SHA-256
+    $keyHash = hash('sha256', $key, true);
+    // Отримуємо довжину вектора ініціалізації (IV)
+    $ivLength = openssl_cipher_iv_length($cipher);
+    // Генеруємо випадковий IV
+    $iv = openssl_random_pseudo_bytes($ivLength);
+    // Шифруємо повідомлення
+    $encrypted = openssl_encrypt($message, $cipher, $keyHash, OPENSSL_RAW_DATA, $iv);
+    // Об'єднуємо IV з зашифрованими даними та кодуємо їх Base64
+    return base64_encode($iv . $encrypted);
 }
 
-// Функція для дешифрування повідомлення
+/**
+ * Функція для дешифрування повідомлення за допомогою AES-256-CBC.
+ */
 function decrypt($encryptedMessage, $key)
 {
-    $encrypted = base64_decode($encryptedMessage);
-    $keyStr = pack('J', $key);
-    $keyLen = strlen($keyStr);
-    $decrypted = '';
-
-    for ($i = 0; $i < strlen($encrypted); $i++) {
-        $encryptedChar = ord($encrypted[$i]);
-        $keyChar = ord($keyStr[$i % $keyLen]);
-        $decrypted .= chr(($encryptedChar - $keyChar + 256) % 256);
-    }
-
-    return $decrypted;
+    $cipher = 'aes-256-cbc';
+    $keyHash = hash('sha256', $key, true);
+    $ivLength = openssl_cipher_iv_length($cipher);
+    // Декодуємо Base64
+    $data = base64_decode($encryptedMessage);
+    // Розділяємо IV і зашифровані дані
+    $iv = substr($data, 0, $ivLength);
+    $encrypted = substr($data, $ivLength);
+    return openssl_decrypt($encrypted, $cipher, $keyHash, OPENSSL_RAW_DATA, $iv);
 }
 
 // Функція для декодування характеристик продукту
